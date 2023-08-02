9b9e9d4a0bc95f55ac0fbf098791d898b6ea9105
usermk001 <useranon790@gmail.com>
2023-08-02 14:05:35 -0400

    n

---
+++ python.py
@@ -0,0 +1,16 @@
+
+"""" "" "" Write a function named integrated that has 1 input (parameter). This parameter's value will be a string. The function should return the parameter's value concatenated with a space and then "besides".
+Sample test cases:
+integrated("real") evaluates to "real besides"
+integrated("other") evaluates to "other besides"
+
+ Write a function named line_eval that takes a float as an input (parameter). This input represents an x-value. Your function should return the position (y-value) on a line 
+ for the input assuming the line has the form m*x + b where m=-0.94 and b=9.15.
+ 
+ 
+ You are building a rectangular pen to contain your pet goats. 
+ You want to test possible pen size so that whatever pen you build will be an appropriate size. 
+ To help you do this, tou decide to write a function which can calculate the area of grass a possible pen would enclose.
+
+Write a function named pen_area that has two inputs (parameters). 
+These parameters will be floats. The first parameter represents the potential pen's length (in meters).The second parameter represents the potential pen's width (in meters). Your function should return the area of the potential goat pen.
