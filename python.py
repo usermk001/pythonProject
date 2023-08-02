78763bd5e03cf0d06151acfd97a7726c63868582
usermk001 <useranon790@gmail.com>
2023-08-02 14:43:51 -0400

    n

--- python.py
+++ python.py
@@ -12,5 +12,17 @@ integrated("other") evaluates to "other besides"
  You want to test possible pen size so that whatever pen you build will be an appropriate size. 
  To help you do this, tou decide to write a function which can calculate the area of grass a possible pen would enclose.
 
+
+
 Write a function named pen_area that has two inputs (parameters). 
 These parameters will be floats. The first parameter represents the potential pen's length (in meters).The second parameter represents the potential pen's width (in meters). Your function should return the area of the potential goat pen.
+
+
+ You are writing a number guessing program where the player must try to guess the secret number 20. This function will take a player's guess and tell them if they are either right or whether they should guess higher or lower on their next turn
+
+Write a function named higher_lower that takes an int as a parameter and returns "higher" if 20 is greater than the input, returns "lower" if 20 is less than the input, and returns "correct" if 20 is equal to the input.
+
+
+ Two people are trying to determine if they are compatible as friends and there is one criteria that is very important to both of them and they must agree on this issue. Conveniently they are able to represent their views as integers and have also determined a boundary integer to determine which side of the issue they are on
+
+Write a function named same_side that takes two integers as parameters. The function will return a boolean which represents true if the inputs are both greater than 48 or if they are both less than 48. If the inputs are not on the same side of 48 or if either input is equal to 48 then the function should return a value representing false
